474d62c07d689e3f454e7a79e9962875b944233c
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-09 12:07:48 +0330

    Added CategorySerializer

--- restaurantAPI/serializers.py
+++ restaurantAPI/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    model = MenuItem
+    fields = ['id', 'slug', 'title']
+
+    class Meta:
+        db_table = 'categories'
+
+
+
