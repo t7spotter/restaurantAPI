a3465e2afc145651f10d0a75e745905398493d8f
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-10 17:58:34 +0330

    Added MenuItem get method

--- restaurantAPI/urls.py
+++ restaurantAPI/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import ListMenuItems
 
+urlpatterns = [
+    path('menu-items', ListMenuItems.as_view()),
+    path('menu-items/<int:pk>', ListMenuItems.as_view()),
 ]
--- restaurantAPI/views.py
+++ restaurantAPI/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+
+class ListMenuItems(APIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: Request, pk=None):
+        if pk:
+            queryset = get_object_or_404(MenuItem, pk=pk)
+            ser = MenuItemSerializer(queryset)
+            return Response(ser.data, status=status.HTTP_200_OK)
+        else:
+            queryset = MenuItem.objects.all()
+            ser = MenuItemSerializer(queryset, many=True)
+            return Response(ser.data, status=status.HTTP_200_OK)
