79467559f3f9dd497023bd4a64d9ddcefc9560ea
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-11 12:28:52 +0330

    Add permission file and IsManager class

---
+++ restaurantAPI/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsManager(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff and request.user.is_superuser)
