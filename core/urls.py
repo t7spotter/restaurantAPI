c381f259e84f2c3b329e8511fd0d58adfe081726
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-13 17:23:03 +0330

    Added obtain token url

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # browsable API login and logout
+    path('token/', views.obtain_auth_token),  # access token endpoint
 
     path('api/', include('restaurantAPI.urls')),
 ]
