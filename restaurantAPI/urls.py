a983c68067be4eae01d0e35c963a0500e48ff342
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-13 13:48:33 +0330

    Edited ManagerGroupManagement url

--- restaurantAPI/urls.py
+++ restaurantAPI/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-from .views import ListMenuItems, ManagerGroupManagement, DeliveryGroupManagement
+from .views import ListMenuItems, ManagerGroupManagement, DeliveryGroupManagement, UserCartManager
 
 urlpatterns = [
     path('menu-items', ListMenuItems.as_view()),
     path('menu-items/<int:pk>', ListMenuItems.as_view()),
 
-
     path('groups/manager/users', ManagerGroupManagement.as_view()),
     path('groups/manager/users/<int:pk>', ManagerGroupManagement.as_view()),
 
-    path('/api/groups/delivery-crew/users', DeliveryGroupManagement.as_view()),
-    path('/api/groups/delivery-crew/users/<int:pk>', DeliveryGroupManagement.as_view()),
+    path('groups/delivery-crew/users', DeliveryGroupManagement.as_view()),
+    path('groups/delivery-crew/users/<int:pk>', DeliveryGroupManagement.as_view()),
+
 ]
