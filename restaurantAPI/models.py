05c44d678ca0212c2e38c4548ddd3cecef640816
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-13 14:48:32 +0330

    Edited Cart Model user field

--- restaurantAPI/models.py
+++ restaurantAPI/models.py
@@ -31,7 +31,7 @@ class MenuItem(models.Model):
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
