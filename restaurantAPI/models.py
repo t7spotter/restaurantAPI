69373b81cbdc86e70ca198ec1248ff112feba503
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-16 13:56:25 +0330

    Transferred get_total_price method to views

--- restaurantAPI/models.py
+++ restaurantAPI/models.py
@@ -37,11 +37,11 @@ class Cart(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    def get_total_price(self):
-        total_order_price = 0
-        for item in self:
-            total_order_price += item.price
-        return total_order_price
+    # def get_total_price(items):
+    #     total_order_price = 0
+    #     for item in items:
+    #         total_order_price += item.price
+    #     return total_order_price
 
     class Meta:
         unique_together = ('menuitem', 'user')
