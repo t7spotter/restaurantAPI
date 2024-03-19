fa83260061c6c3034e60c99a67f68f3feff7fa8e
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-19 22:50:51 +0330

    Added IsDeliveryCrew permission calss

--- restaurantAPI/permissions.py
+++ restaurantAPI/permissions.py
@@ -5,3 +5,9 @@ class IsManager(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_staff and request.user.is_superuser)
+
+
+class IsDeliveryCrew(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.groups.filter(name='delivery'))
