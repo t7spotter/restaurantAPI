dbbd8e380cfdcf0d9884221a6bccac786ec1ffc8
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-03-31 00:48:46 +0330

    Added ready method for config signal in app.py

--- restaurantAPI/apps.py
+++ restaurantAPI/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RestaurantapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restaurantAPI'
+
+    def ready(self):
+        import restaurantAPI.signals
