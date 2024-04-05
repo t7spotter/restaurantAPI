5b94b592a2850aef8bdbd16058461b3e4d151747
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-04-05 18:02:39 +0330

    Modified create_groups file print log

--- restaurantAPI/management/commands/create_groups.py
+++ restaurantAPI/management/commands/create_groups.py
@@ -2,11 +2,11 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 
 
-class Command(BaseCommand):   # Should name it "Command"
+class Command(BaseCommand):  # Should name it "Command"
     help = 'Create default groups'
 
     def handle(self, *args, **options):
         groups = ['delivery', 'manager', 'customer']
         for group_name in groups:
             Group.objects.get_or_create(name=group_name)
-        self.stdout.write(self.style.SUCCESS('Groups created successfully'))
+        self.stdout.write(self.style.SUCCESS('"manager", "delivery" and "customer" groups created successfully'))
