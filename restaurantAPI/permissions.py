c1d2e10f8311b0a6bfad938d2654d7ea428208b5
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-05-06 18:13:30 +0330

    Created IsAuthenticatedOrReadOnly permission class

--- restaurantAPI/permissions.py
+++ restaurantAPI/permissions.py
@@ -1,6 +1,9 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, IsAuthenticated
 
 from restaurantAPI.models import OrderItem
+
+MANAGER_METHODS = ('POST', 'PUT', 'DELETE')
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
 
 
 class IsManager(BasePermission):
@@ -31,5 +34,34 @@ class IsCustomerAndHasBoughtItem(BasePermission):
                 has_ordered_item = OrderItem.objects.filter(order__user=request.user, menuitem=item_id).exists()
                 return has_ordered_item
             return False
+        else:
+            return False
+
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    """
+    The request is authenticated as a user, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated
+        )
+
+
+class IsManagerOrCustomerReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in MANAGER_METHODS:
+            if bool(request.user and
+                    request.user.is_staff and
+                    request.user.is_superuser and
+                    request.user.groups.filter(name='manager').exists()):
+                return True
+            return False
+        elif request.method in SAFE_METHODS:
+            is_authenticated = IsAuthenticated()
+            return is_authenticated.has_permission(request, view)
         else:
             return False
